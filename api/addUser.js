c079a3a5b1e4140a50ab2cbc5a37725a96653c35
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2025-05-05 15:54:18 +0530

    Update addUser.js

--- api/addUser.js
+++ api/addUser.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors"; // ← import cors
 import { connectDB } from "../config/db.js";
 import User from "../models/User.js";
 import { successResponse, errorResponse } from "../utils/response.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.post("/api/addUser", async (req, res) => {
