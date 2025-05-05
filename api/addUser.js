af1f1081d2c849db78576e85ca0f4d25ba0ca956
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2025-05-05 16:19:41 +0530

    Update addUser.js

--- api/addUser.js
+++ api/addUser.js
@@ -5,10 +5,16 @@ import User from "../models/User.js";
 import { successResponse, errorResponse } from "../utils/response.js";
 
 const app = express();
-
-app.use(cors());
-app.options("*", cors()); // allow OPTIONS preflight
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // or restrict to a specific domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.post("/api/addUser", async (req, res) => {
   try {
